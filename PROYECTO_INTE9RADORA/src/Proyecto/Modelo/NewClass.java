7928e199e548d61f900d018e4cbf56dd02bf2629
PepeChuy <rgppchuy@gmail.com>
2015-10-23 17:31:06 -0500

    PROYECTO_INTEGRADORA_V1

---
+++ PROYECTO_INTE9RADORA/src/Proyecto/Modelo/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proyecto.Modelo;
+
+/**
+ *
+ * @author toño
+ */
+public class NewClass {
+    
+}
